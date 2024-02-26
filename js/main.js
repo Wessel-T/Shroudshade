aa3488b42c3619650465d95dfa06ae3b9565a22a
wessel Tichelaar <302524453@student.rocmondriaan.nl>
2024-02-26 13:32:35 +0100

    wowzers

--- js/main.js
+++ js/main.js
@@ -11,12 +11,23 @@ var config = {
 
 var game = new Phaser.Game(config);
 
+
 function preload ()
 {
+    this.load.image('sky', 'img/sky.png');
+    this.load.image('ground', 'img/platform.png');
+    this.load.image('star', 'img/star.png');
+    this.load.image('bomb', 'img/bomb.png');
+    this.load.spritesheet('dude', 
+        'img/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
 }
 
 function create ()
 {
+    this.add.image(400, 300, 'sky');
+    this.add.image(400, 300, 'star');
 }
 
 function update ()
